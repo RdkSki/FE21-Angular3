307fceddb59491c9417eb47a92d3ae157a556abb
RdkSki <rotax@gmx.net>
2021-10-13 11:43:45 +0200

    starting

---
+++ petshop/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PetListComponent } from './pet-list/pet-list.component';
+import { PetDetailsComponent } from './pet-details/pet-details.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PetListComponent,
+    PetDetailsComponent,
+    FooterComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
